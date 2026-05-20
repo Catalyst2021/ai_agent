0d6fce45c6b12536b898f20ae1455255f97d293b
Catalyst2021 <Anddrew.Le@gmail.com>
2026-05-20 12:54:04 -0400

    add multiple messages to agent

--- main.py
+++ main.py
@@ -2,33 +2,40 @@ import os
 from dotenv import load_dotenv
 import argparse
 from google import genai
+from google.genai import types
 
-
+#Load enviroment variables 
 load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-
-if api_key is None: 
-    raise RuntimeError("Missing API key")
-
-client = genai.Client(api_key=api_key)
-
-user_prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-
-response = client.models.generate_content(
-    model='gemini-2.5-flash', contents= user_prompt
-)
-if response.usage_metadata is None: 
-    raise RuntimeError("Failed API request")
-
-print(f"User prompt: {user_prompt}")
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-print(f"Response: {response.text}")
-
 
 
 def main():
     print("Hello from ai-agent!")
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+
+    api_key = os.environ.get("GEMINI_API_KEY")
+
+    if api_key is None: 
+        raise RuntimeError("Missing API key")
+
+    messages: list[types.Content] = [
+        types.Content(role="user", parts=[types.Part(text=args.user_prompt)])
+    ]
+
+    client = genai.Client(api_key=api_key)
+
+    response = client.models.generate_content(
+        model='gemini-2.5-flash', contents= messages
+    )
+    if response.usage_metadata is None: 
+        raise RuntimeError("Failed API request")
+
+    print(f"User prompt: {args.user_prompt}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print(f"Response: {response.text}")
+
 
 
 if __name__ == "__main__":
